26db47e8eef04215bf581e853c7ac8878552a245
FranciscoTxz <angelftv2002@hotmail.com>
2026-09-09 16:06:40 -0600

    fix: Constants fixed

--- src/common/constants.py
+++ src/common/constants.py
@@ -1,6 +1,6 @@
 import os
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
 MONGODB_URI = os.getenv("MONGODB_URI")
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
 SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
